057fe13746a88c5a5c2d3630409ce8d590615820
David Harlow <davidharlow@redweb.com>
2019-04-26 13:38:02 +0100

    absolutely fuming

--- js/main.js
+++ js/main.js
@@ -3,4 +3,6 @@ document.getElementsByClassName("rocket")[0].addEventListener("click", addFly);
 function addFly() {
     var rocket = document.getElementsByClassName("rocket-body");
     rocket[0].classList.add("fly");
+    var fumes = document.getElementsByClassName("fumes");
+    fumes[0].classList.add("exhaust-fumes");
 }
